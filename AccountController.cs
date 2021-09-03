8290cd40201b9a6cde6fdf475e0ce975f6998d23
Karizsa <90028864+Karizsa@users.noreply.github.com>
2021-09-03 15:49:52 +0800

    Added Coding Exam files

---
+++ AccountController.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Web.Http;
+using System.Web.Http.Description;
+using CodingExam.Models;
+
+
+namespace CodingExam.Controllers
+{
+    public class AccountController : ApiController
+    {
+        private AccountDbContext db = new AccountDbContext();
+        
+        // GET: api/Account
+   
+        public IEnumerable<vAccountDetail> GetAccount()
+        {
+            return db.vAccountDetails;
+        }
+
+        
+    }
+}
